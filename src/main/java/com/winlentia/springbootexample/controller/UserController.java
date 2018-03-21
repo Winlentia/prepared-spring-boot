ba852303eb079f078bd3e918662a83b8a23bed1d
winlentia <winlentia@gmail.com>
2018-03-21 15:52:35 +0300

    request mapping get method by id created

--- src/main/java/com/winlentia/springbootexample/controller/UserController.java
+++ src/main/java/com/winlentia/springbootexample/controller/UserController.java
@@ -7,6 +7,7 @@ import javax.xml.ws.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,5 +32,11 @@ public class UserController
         return new ResponseEntity<List<User>>(users, HttpStatus.OK);
     }
 
-    
+    @RequestMapping(value = "/users/{id}/" , method = RequestMethod.GET)
+    public ResponseEntity<?> getUser(@PathVariable("id") long id ){
+        User user =  userService.findById(id);
+        return new ResponseEntity<User>(user , HttpStatus.OK);
+    }
+
+
 }
