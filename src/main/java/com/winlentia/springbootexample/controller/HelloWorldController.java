0d888285f8b613ba6ea6ad1ff0d0d376c8baaae3
winlentia <winlentia@gmail.com>
2018-03-21 11:07:07 +0300

    .

--- src/main/java/com/winlentia/springbootexample/controller/HelloWorldController.java
+++ src/main/java/com/winlentia/springbootexample/controller/HelloWorldController.java
@@ -2,11 +2,8 @@ package com.winlentia.springbootexample.controller;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.winlentia.springbootexample.entity.Greeting;
